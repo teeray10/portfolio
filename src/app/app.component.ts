a3e5aec7ee86c251ea2b9f20047485d0802f5f7c
Taylor <taylorray.tray@gmail.com>
2024-12-20 14:16:22 +0200

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,16 @@
-import { AfterViewChecked, Component, HostListener, inject } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  HostListener,
+  inject,
+} from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
-import { AboutComponent } from '@home/components/about/about.component';
-import { ExperienceComponent } from '@home/components/experience/experience.component';
-import { ProjectsComponent } from '@home/components/projects/projects.component';
-import { SidebarComponent } from '@home/components/sidebar/sidebar.component';
-import { SkillsComponent } from '@home/components/skills/skills.component';
-import { IsVisibleDirective } from 'src/app/directives/is-visible.directive';
-import { ArrowUpIconComponent } from 'src/app/features/home/icons/arrow-up-icon/arrow-up-icon.component';
-import { VisibleContentService } from 'src/app/features/home/utility/visible-content.service';
+import { VisibleContentService } from '@home/utility/visible-content.service';
 
 @Component({
   selector: 'tr-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, ProjectsComponent, ExperienceComponent, SkillsComponent, AboutComponent, IsVisibleDirective, ArrowUpIconComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent implements AfterViewChecked {
@@ -34,7 +32,7 @@ export class AppComponent implements AfterViewChecked {
     this.visibleContentService.setVisibleContent(this.route.snapshot.fragment);
   }
 
-  mouseBg(): string {    
+  mouseBg(): string {
     return this.background;
   }
 }
