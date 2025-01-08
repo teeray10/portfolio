f3f5f6ad68da93c529d2ee553963394947f3e6b2
Taylor <taylorray.tray@gmail.com>
2025-01-08 14:52:36 +0000

    Add Goodreads and update About me section

--- src/app/pages/home/components/social/social.component.ts
+++ src/app/pages/home/components/social/social.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { GithubIconComponent } from '@pages/home/icons/github-icon/github-icon.component';
+import { GoodreadsIconComponent } from '@pages/home/icons/goodreads-icon/goodreads-icon.component';
 import { LinkedinIconComponent } from '@pages/home/icons/linkedin-icon/linkedin-icon.component';
 
 @Component({
   selector: 'tr-social',
   standalone: true,
-  imports: [GithubIconComponent, LinkedinIconComponent],
+  imports: [GithubIconComponent, LinkedinIconComponent, GoodreadsIconComponent],
   templateUrl: './social.component.html',
 })
 export class SocialComponent {}
---
+++ src/app/pages/home/icons/goodreads-icon/goodreads-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tr-goodreads-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './goodreads-icon.component.html',
+  styles: ``
+})
+export class GoodreadsIconComponent {
+
+}
